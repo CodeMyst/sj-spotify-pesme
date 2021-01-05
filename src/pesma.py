a6dcf812f76a875db41624b2e892308c0c80b8a0
CodeMyst <code@myst.rs>
2021-01-05 16:06:48 +0100

    ispisuje duzinu u fajl u ms
    - obrisane neke funkcije koje se ne koriste

--- src/pesma.py
+++ src/pesma.py
@@ -1,14 +1,3 @@
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 class Pesma:
     # duzina je u ms
     def __init__(self, ime, autor, album, godina, duzina):
@@ -36,24 +25,4 @@ class Pesma:
     # ako bi ime/autor/album sadrzali taj karakter
     def toLongString(self):
         return self.ime + "\n" + self.autor + "\n" + self.album + "\n" + \
-            self.godina + "\n" + self.vremeString() + "\n\n"
-
-    # lepsi prikaz pesme, prema sledecem formatu:
-    # ime - autor - album - vreme
-    def toShortString(self):
-        ime = self.ime
-        autor = self.autor
-        album = self.album
-        vreme = self.vremeString()
-
-        if len(ime) > 37:
-            ime = ime[:37] + "..."
-
-        if len(autor) > 27:
-            autor = autor[:27] + "..."
-
-        if len(album) > 27:
-            album = album[:27] + "..."
-
-        return ("│  " + bcolors.OKBLUE + bcolors.BOLD + "{:<40}" + bcolors.ENDC + "  │   " + bcolors.OKBLUE + "{:<30}" + bcolors.ENDC + "   │   " + bcolors.OKBLUE + "{:<30}" + bcolors.ENDC + "   │  " + bcolors.FAIL + "{:<4}" + bcolors.ENDC + "  │  " + bcolors.FAIL + "{:<5}" + bcolors.ENDC + "  │") \
-            .format(ime, autor, album, self.godina, vreme)
+            self.godina + "\n" + str(self.duzina) + "\n\n"
